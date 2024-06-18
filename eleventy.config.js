3df373030499e490e9385c49f78fdfb45d7c9d29
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2024-06-18 16:00:02 +0800

    Add CSS post-processing with Lightning CSS
    This commit adds `.css` as a custom template extension to eleventy to be
    bundled by lightningcss.
    
    Per Sass convention, css files that start with an underscore are treated
    as partials and therefore not compiled into the output folder. I also
    excluded css files inside a directory starting with an underscore from
    the build.

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,6 +1,10 @@
+import lightningcss from "./config/lightningcss.js"
+
 /** @param {import("@11ty/eleventy").UserConfig} eleventyConfig */
 export default function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy('src/assets/images')
+
+  eleventyConfig.addPlugin(lightningcss)
 
   return {
     dir: {
